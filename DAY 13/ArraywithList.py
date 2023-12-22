6acccd9bf02e0a537d6b1f0240cbee9a66f73a7a
aparna62 <106722318+aparna62@users.noreply.github.com>
2023-12-22 11:59:18 +0530

    Delete DAY 13 directory

--- DAY 13/ArraywithList.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-array=np.array([[6,4,3],[5,3,2]],dtype='float')
-print("array from list")
-print(array)
-#creating array with tuple
-array1=np.array((1,2,3))
-print("Array from tuple")
-print(array1)
