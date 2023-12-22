ba0fb91dc8a8614cfd8717e05360715af3a09612
aparna62 <106722318+aparna62@users.noreply.github.com>
2023-12-22 11:59:42 +0530

    Delete DAY 14 directory

--- DAY 14/JSONFILE.py
+++
@@ -1,6 +0,0 @@
-import json
-json_String='{"id":101,"name":"aparna","age":20}'
-data=json.loads(json_String)
-print(data)
-print(data['name'])
-print(data['age'])
--- DAY 14/JsonStringConvert.py
+++
@@ -1,4 +0,0 @@
-import json
-json_String='{"id":101,"name":"aparna","age":30}'
-data=json.loads(json_String)
-print(data)
