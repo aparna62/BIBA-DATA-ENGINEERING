f5c8b9e38a89c12e573af83d6e93e472cefa574f
aparna62 <106722318+aparna62@users.noreply.github.com>
2023-12-22 11:58:52 +0530

    Delete DAY 11 directory

--- DAY 11/FunctionDemo.py
+++
@@ -1,4 +0,0 @@
-def student(firstname, lastname):
-    print(firstname, lastname)
-student(firstname='Hexa', lastname='Practice')
-student(lastname='Practice', firstname='Hexa')
--- DAY 11/PrintsumofNnaturalnumbers.py
+++
@@ -1,9 +0,0 @@
-
-def findsum(n):
-    sum1=0
-    for i in range(1,n+1):
-        sum1=sum1+i
-    print(sum1)
-n=int(input())
-print(findsum(n))
-    
