0fb4d73c2d8551518b2b9eda949c7868724668a2
aparna62 <106722318+aparna62@users.noreply.github.com>
2023-12-22 11:59:04 +0530

    Delete DAY 12 directory

--- DAY 12/CSV3.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-filepath='c:\\Users\\Aparna\\Desktop\\Hexaware Python\\CSVDemo\\salary_data.csv'
-data= pd.read_csv(filepath)
-print(data)
-print(data)
--- DAY 12/UsingDictReader.py
+++
@@ -1,7 +0,0 @@
-import csv
-filepath='C:\\Users\\Aparna\\Desktop\\Hexaware Python\\CSVDemo\\Salary_Data.csv'
-with open(filepath,'r') as csvfile:
-    reader=csv.DictReader(csvfile)
-    for row in reader:
-        print(row)
-    
